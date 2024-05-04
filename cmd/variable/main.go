1693772efedd99c6f4961406e9bac74e5f59b024
thetinshusasi <thetinshusasi@gmail.com>
2024-05-04 08:06:09 -0300

    feat(app): ticket 1 variables
    variables desc

--- cmd/variable/main.go
+++ cmd/variable/main.go
@@ -9,6 +9,25 @@ func main() {
 	var boolVal bool
 	var stringVal string
 
-	fmt.Println("%v %f %v %q \n", intVal, floatVal, boolVal, stringVal)
+	fmt.Printf("%v %f %v %q \n", intVal, floatVal, boolVal, stringVal)
 
+	intVal2 := 100
+
+	congrats := " happy birthday"
+
+	fmt.Printf("%v %s \n", intVal2, congrats)
+
+	fmt.Printf("%T %T %T %T \n", intVal, floatVal, boolVal, stringVal)
+
+	fmt.Println(intVal, "sdafsdf ", intVal2)
+
+	// constants
+
+	const constVal = "This is a constant"
+	// constVal = " this won't work"
+	fmt.Printf("%s \n", constVal)
+
+	if true {
+		fmt.Println("conditonal examples")
+	}
 }
